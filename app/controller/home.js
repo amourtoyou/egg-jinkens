a9dab47cdace7c461267b4960235faecb3c83960
amourtoyou <1012890246@qq.com>
2021-06-13 21:41:10 +0800

    q

--- app/controller/home.js
+++ app/controller/home.js
@@ -2,7 +2,7 @@ const Controller = require("egg").Controller;
 
 class HomeController extends Controller {
   async index() {
-    this.ctx.body = "Hello world jinkens";
+    this.ctx.body = "Hello world jinkens two";
   }
 }
 
