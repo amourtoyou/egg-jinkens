761503147f2755531d6f4a3e1e8070da4a9482c7
amourtoyou <1012890246@qq.com>
2021-10-31 17:20:59 +0800

    push orgin

--- app/controller/home.js
+++ app/controller/home.js
@@ -3,6 +3,7 @@ const Controller = require("egg").Controller;
 class HomeController extends Controller {
   async index() {
     console.log('stage-dev123');
+    console.log('push');
     this.ctx.body = "Hello world jinkens two agins";
   }
 }
